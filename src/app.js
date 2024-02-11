a2472da5147ab6caf9e0dfe720f76929f5bddf1c
NarendraDeoSingh <singh.narendradeo@gmail.com>
2024-02-11 18:07:58 +0530

    cloudinary setup and routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 const app = express();
 
 app.use(
@@ -15,5 +14,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
